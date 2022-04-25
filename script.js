a7fbf78a0cd19a8f98afafb3813d91b6c4094275
tikokorghanashvili <101791266+tikokorghanashvili@users.noreply.github.com>
2022-04-25 21:49:51 +0400

    add

---
+++ script.js
@@ -0,0 +1,58 @@
+// // 1 დავალება
+// let array= [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+// array.push( 'javascript' , 'python');
+// array.unshift( 'html', 'css');
+// // 3 პირობა მაკლია
+// array.shift();
+// array.pop();
+
+// console.log(array);
+
+// // 2 მაგალითი
+// let names= ['Orange','banana','pear'];
+// // 1 პირობა მაკლია
+// names.push('apple','Pineapple');
+// names.unshift('Watermelon');
+// // 4 პირობა მაკლია
+// names.splice(2,0,'mango');
+// names.shift();
+// names.pop();
+// // 8 პირობა მაკლია
+
+// console.log(names);
+
+// // 3 მაგალითი
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+
+// // 4 მაგალითი
+// let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+// let newarray=array.filter(item => item % 2 ==1);
+
+// არ მაქვს დასრულები მხოლოდ კენტები გამომაქვს ჯერ
+
+// console.log(newarray);
+
+// // 5 მაგალითი
+// let languages = ['html', 'css', 'javascript', 'python','php'];
+// let newlanguages=languages.filter(x => x.length>3);
+
+// console.log(newlanguages);
+
+// // 6 მაგალითი
+// let array= ['academy', 'of', 'digital', 'industries'];
+
+// არ მაქვს დასრულებული reduce
+
+
+// // 7 მაგალითი
+// let item = [12, 'google', 32, null, 'yahoo', 25];
+
+// არ მაქვს დასრულებული Map
+
+
+// 8 მაგალითი
+let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+let newwords=words.filter(x=> x.includes('M'));
+არ მაქვს დასრულებული მხოლოდ M შეიცავს გამომაქვს
+
+console.log(newwords);
